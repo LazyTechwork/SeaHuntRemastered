78bdd12e9082509090c88fb1cffdc09ec25bfe26
LazyTechwork <lazytechwork@gmail.com>
2019-03-02 14:05:34 +0300

    Image load + player showed

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,35 @@
 // Canvas and context definitions
 var canvas = document.getElementById("game");
+$("body").hide();
 var ctx = canvas.getContext("2d");
 
+var images = [];
 
+// Utilities
+function loadImages(imageArray) {
+    let loaded = 0, needToBeLoaded = 0;
+    for (let key in imageArray) {
+        if (!imageArray.hasOwnProperty(key)) continue;
+        needToBeLoaded++;
+        console.log(imageArray[key] + " " + key);
+        images[key] = new Image();
+        images[key].src = "assets/img/" + imageArray[key];
+        images[key].onload = function () {
+            loaded++;
+            if (loaded >= needToBeLoaded) $("body").fadeIn();
+        };
+    }
+}
+
+loadImages({
+    "player": "player.png",
+    "shark": "shark.png",
+    "fish": "fish.png"
+});
+
+function render() {
+    ctx.drawImage(images['player'], 10, 10, images['player'].width * .8, images['player'].height * .8);
+    window.requestAnimationFrame(render);
+}
+
+window.requestAnimationFrame(render);
